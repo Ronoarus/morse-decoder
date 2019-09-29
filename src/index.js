a423e5c9d446de3ac6a153a7a741011881ea4211
ronoarus <denis.poleschyk@gmail.com>
2019-09-29 13:55:08 +0400

    feat: task complet

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result='';
+    let temp,temp1='';
+    for(let i=0;i<expr.length;i+=10){   
+        temp=expr.slice(i,i+10);   
+        temp=temp.padStart(10, '0');     
+        if(temp[0]=='*'){
+            result+=' ';
+        } else{
+            for(let j=0;j<10;j+=2){
+                if(temp.slice(j,j+2)=='10') {
+                    temp1+='.';
+                }
+                else if(temp.slice(j,j+2)=='11'){
+                    temp1+='-';
+                }                
+            }
+            result+=MORSE_TABLE[temp1];
+            temp1='';
+        }    
+    }  
+    return result;
 }
 
 module.exports = {
